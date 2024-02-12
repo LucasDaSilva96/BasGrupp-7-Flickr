b4b12864043e6d83d9a7c8e5e1ea76445f826b20
Mayonesa90 <mayonaas@hotmail.com>
2024-02-12 12:04:19 +0100

    added js code

--- js/pagination.js
+++ js/pagination.js
@@ -1,12 +1,13 @@
 const startBtn = document.querySelector("#pagination-sec_startBtn"),
 endBtn = document.querySelector("#pagination-sec_endBtn"),
-prevNext = document.querySelectorAll("#pagination-sec_prevNext"),
+prevNext = document.querySelectorAll(".pagination-sec_prevNext"),
 numbers = document.querySelectorAll(".pagination-sec_link");
 
 let currentStep = 0;
 
 numbers.forEach((number, numIndex) => {
-  number.addEventListener("click", () => {
+  number.addEventListener("click", (e) => {
+    e.preventDefault();
     currentStep = numIndex;
 
     document.querySelector(".pagination-sec_active").classList.remove("pagination-sec_active");
@@ -14,4 +15,14 @@ numbers.forEach((number, numIndex) => {
     number.classList.add("pagination-sec_active");
 
   })
+})
+
+prevNext.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        currentStep += e.target.id === "next" ? 1 : -1;
+        console.log(currentStep);
+        numbers.forEach((number, numIndex) => {
+          
+        })
+      })
 })
