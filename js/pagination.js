672fa1857e7c283e2a1db226e4c73a2f98d9ede6
Mayonesa90 <mayonaas@hotmail.com>
2024-02-12 16:24:45 +0100

    added code to pagination js-file

--- js/pagination.js
+++ js/pagination.js
@@ -1,3 +1,5 @@
+// BUTTONS
+
 const startBtn = document.querySelector("#pagination-sec_startBtn"),
 endBtn = document.querySelector("#pagination-sec_endBtn"),
 prevNext = document.querySelectorAll(".pagination-sec_prevNext"),
@@ -30,13 +32,14 @@ numbers.forEach((number, numIndex) => {
     number.classList.add("pagination-sec_active");
 
     updateBtn();
+        
+    searchImage(SEARCH_PARAM, 12, currentStep++, toasts); // uppdatera funktionen för att hämta nästa sida - HUR?
   })
 })
 
 prevNext.forEach((button) => {
     button.addEventListener("click", (e) => {
         currentStep += e.target.id === "next" ? 1 : -1; 
-        console.log(currentStep);
         numbers.forEach((number, numIndex) => {
           number.classList.toggle("pagination-sec_active", numIndex === currentStep)
           updateBtn();
@@ -61,3 +64,16 @@ endBtn.addEventListener("click", () => {
   startBtn.disabled = false;
   prevNext[0].disabled = false;
 })
+
+// IMAGES
+
+async function replaceImages(replacement) {
+
+  const images = document.querySelectorAll(".pagination-sec_img");
+  const imagesResult = replacement;
+
+  images.forEach((img, i) => {
+    img.src = imagesResult[i];
+  })
+
+}
