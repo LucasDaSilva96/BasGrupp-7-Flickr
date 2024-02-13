db6b9f46f60a73603fbc72664ed666d6e7940a53
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-02-13 21:45:46 +0100

    Created a simple example of pagination for education purpose

--- main.js
+++ main.js
@@ -8,10 +8,27 @@ const searchbar = document.querySelector(".search-sec__search-box__input");
 const searchIcon = document.getElementById("search-btn");
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
+// --------------- Pagination-selection ---------------------
+const pagination_wrapper = document.querySelector(
+  ".pagination-sec-img-wrapper"
+);
+const prev_button = document.getElementById("prev-btn");
+const next_button = document.getElementById("next-btn");
+const page_display_nr = document.getElementById("display-nr");
+
 // ************ Global variables ***********
+// ---------------- Pagination object -----------------
+const PAGINATION_PAGE_OBJ = {
+  "page-1": [],
+  "page-2": [],
+  "page-3": [],
+  "page-4": [],
+  "page-5": [],
+};
+// -----------------------------------------------------
 let currentPageNumber = 1;
 const amountOfImagePerPage = 12;
-let currentSearch = "Iphone";
+let currentSearch = "God morning";
 const toasts = new Toasts({
   offsetX: 20, // 20px
   offsetY: 20, // 20px
@@ -71,6 +88,8 @@ async function searchDefaultImages() {
       currentPageNumber,
       toasts
     );
+    PAGINATION_PAGE_OBJ["page-1"] = result;
+    displayResult(PAGINATION_PAGE_OBJ["page-1"]);
   } catch (error) {
     toasts.push({
       title: "Fetch status",
@@ -95,6 +114,11 @@ async function searchImages(currentSearch, num_per_page, page_num, toasts) {
       page_num,
       toasts
     );
+    clearPaginationObj();
+    PAGINATION_PAGE_OBJ["page-1"] = result;
+    pagination_wrapper.innerHTML = "";
+    displayResult(PAGINATION_PAGE_OBJ["page-1"]);
+    scrollIntoView(page_display_nr);
   } catch (error) {
     toasts.push({
       title: "Fetch status",
@@ -121,7 +145,7 @@ searchbar.addEventListener("input", (e) => (currentSearch = e.target.value));
 // ******* Search - eventListener **
 searchIcon.addEventListener("click", async () => {
   currentPageNumber = 1;
-
+  page_display_nr.textContent = currentPageNumber;
   await searchImages(
     currentSearch,
     amountOfImagePerPage,
@@ -131,10 +155,59 @@ searchIcon.addEventListener("click", async () => {
 });
 
 // **** Scroll node-element-into-view function
-function ScrollIntoView(nodeEl) {
+function scrollIntoView(nodeEl) {
   return nodeEl.scrollIntoView({
     behavior: "smooth",
     block: "end",
     inline: "end",
   });
+}
+
+// ******************************* Pagination Example ************
+
+// Display images ↓
+function displayResult(result) {
+  result.forEach((imgUrl) => {
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    pagination_wrapper.appendChild(img);
+  });
+}
+
+// Next page logic ↓
+next_button.addEventListener("click", async () => {
+  currentPageNumber =
+    currentPageNumber < 5 ? currentPageNumber + 1 : currentPageNumber;
+
+  next_button.disabled = true;
+  if (PAGINATION_PAGE_OBJ[`page-${currentPageNumber}`].length === 0) {
+    page_display_nr.textContent = "Loading...";
+    const result = await fetchPagination(
+      currentSearch,
+      amountOfImagePerPage,
+      currentPageNumber
+    );
+    PAGINATION_PAGE_OBJ[`page-${currentPageNumber}`] = result;
+  }
+  pagination_wrapper.innerHTML = "";
+  displayResult(PAGINATION_PAGE_OBJ[`page-${currentPageNumber}`]);
+  page_display_nr.textContent = currentPageNumber;
+  next_button.disabled = false;
+});
+
+// Previous page logic ↓
+prev_button.addEventListener("click", () => {
+  currentPageNumber =
+    currentPageNumber > 1 ? currentPageNumber - 1 : currentPageNumber;
+  pagination_wrapper.innerHTML = "";
+
+  displayResult(PAGINATION_PAGE_OBJ[`page-${currentPageNumber}`]);
+  page_display_nr.textContent = currentPageNumber;
+});
+
+// Clear PAGINATION_PAGE_OBJ function ↓
+function clearPaginationObj() {
+  for (const property in PAGINATION_PAGE_OBJ) {
+    PAGINATION_PAGE_OBJ[property] = [];
+  }
 }
