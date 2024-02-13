43e2cf44839844941d70ca93933c67d397640500
Emma <emma.dybdorf@hotmail.com>
2024-02-13 14:54:35 +0100

    Lagt till i min branch html, css, js, Darkmode

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 "use-strict"; // Strict mode
+
+// Kolla om 'darkMode' är sparad i localStorage
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+// Lägg till klassen till body-elementet
+document.body.classList.add('darkmode');
+// Uppdatera darkMode i localStorage
+localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+// Ta bort klassen från body-elementet
+document.body.classList.remove('darkmode');
+// Uppdatera darkMode i localStorage
+localStorage.setItem('darkMode', null);
+}
+
+// Om användaren redan har besökt och aktiverat darkMode
+// starta med det aktiverat
+if (darkMode === 'enabled') {
+enableDarkMode();
+}
+
+// När någon klickar på knappen
+darkModeToggle.addEventListener('click', () => {
+// Hämta deras darkMode-inställning
+darkMode = localStorage.getItem('darkMode');
+
+// Om den inte är aktiverad, aktivera den
+if (darkMode !== 'enabled') {
+enableDarkMode();
+// Om den har aktiverats, stäng av den
+} else {
+disableDarkMode();
+}
+});
+
