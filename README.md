30763032475242ffb550e29f36d543aa6708ce8c
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-02-14 21:04:52 +0100

    Fixed some bugs, added more style and created the README file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vi är grupp 7 och vi är glada att få presentera vårt arbete med Flickr API.
+
+- Lucas,
+- Emma,
+- Tor,
+- Maya,
+- Ellika
+
+### Vi har skapat en användarvänlig webbapplikation som möjliggör sökning och visning av bilder från Flickr.
+
+![Preview-image-1](/images/flickr-1.png)
+![Preview-image-2](/images/flickr-2.png)
+![Preview-image-3](/images/flickr-3.png)
+![Preview-image-4](/images/flickr-4.png)
--- main.js
+++ main.js
@@ -50,7 +50,7 @@ const swiper = new Swiper(".swiper", {
     slideShadows: true,
   },
   autoplay: {
-    delay: 3000,
+    delay: 2000,
     disableOnInteraction: false,
   },
 
@@ -410,44 +410,44 @@ const clickedImage = document.createElement("img");
 //function that finds what image the user clicks on
 
 images.forEach((img) => {
-     img.addEventListener("click", (e) => {
-         imgSrc = e.target.src;
-         selectedImage = imgSrc;
-        
-        //show modal
-         modal.style.display = "initial";
+  img.addEventListener("click", (e) => {
+    imgSrc = e.target.src;
+    selectedImage = imgSrc;
 
-        //send clicked image source to modal and create a new element
-        clickedImage.setAttribute("src", `${selectedImage}`);
-        clickedImage.setAttribute("class","modal-image");
-        modalItem.appendChild(clickedImage);
+    //show modal
+    modal.style.display = "initial";
 
-        //Make it so the user can't scroll behind the modal
-        document.body.style.overflow ="hidden";
-    });
+    //send clicked image source to modal and create a new element
+    clickedImage.setAttribute("src", `${selectedImage}`);
+    clickedImage.setAttribute("class", "modal-image");
+    modalItem.appendChild(clickedImage);
+
+    //Make it so the user can't scroll behind the modal
+    document.body.style.overflow = "hidden";
+  });
 });
 
-
 //close function for modal
-const close = document.querySelector(".modal__content__close");
+const close = document.querySelector(".modal__iconWrapper");
 
 const modalClose = () => {
-    modal.style.display ="none";
-    clickedImage.removeAttribute("src");
-    document.body.style.overflow = "initial";
+  restartSlide();
+  modal.style.display = "none";
+  clickedImage.removeAttribute("src");
+  document.body.style.overflow = "initial";
 };
 
 close.addEventListener("click", modalClose);
 
 //close if click outside of modal
-window.onclick = function(e) {
-    if (e.target === modal) {
-         modalClose();
-        }
-}
+window.onclick = function (e) {
+  if (e.target === modal) {
+    modalClose();
+  }
+};
 //close if click escape on keyboard
 document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-        modalClose();
-    }
-})
+  if (e.key === "Escape") {
+    modalClose();
+  }
+});
